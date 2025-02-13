24476f5fd77875e8167b3bc2aa6c98bac4dadc6a
dmitry <dmitry_romanof@mail.ru>
2025-02-13 17:26:54 +0500

    processing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_card_number, get_mask_account
 from src.widget import mask_account_card, get_date
+from src.processing import filter_by_state, sort_by_date
 
 
 if __name__ == "__main__":
@@ -14,3 +15,20 @@ if __name__ == "__main__":
     print(mask_account_card("Visa Gold 5999414228426353"))
     print(mask_account_card("Visa Gold 5999414228426353"))
     print(get_date("2024-03-11T02:26:18.671407"))
+
+
+# Пример использования функций
+transactions_data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Используем filter_by_state
+filtered_executed = filter_by_state(transactions_data)
+print(filtered_executed)
+
+# Используем sort_by_date
+sorted_transactions = sort_by_date(transactions_data)
+print(sorted_transactions)
--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,15 @@
-def filter_by_state(transactions: list[dict], state: str='EXECUTED'):
+def filter_by_state(transactions_list, state='EXECUTED'):
     """
     Фильтрует список словарей по значению ключа 'state'.
     """
-    return [transaction for transaction in transactions if transaction["state"] == state]
+    return [transaction for transaction in transactions_list if transaction["state"] == state]
 
 
-def sort_by_date(transactions, reverse=True):
+def sort_by_date(transactions_list, reverse=True):
     """
     Сортирует список словарей по ключу 'date'.
+
     """
-    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
-
-
-# Пример использования
-if __name__ == "__main__":
-    transactions = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-    print(filter_by_state(transactions))
-    print(sort_by_date(transactions))
+    return sorted(transactions_list, key=lambda x: x['date'], reverse=reverse)
 
 
