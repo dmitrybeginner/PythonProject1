c8148a40a95a9fa92c541b0cadc9035617a94d0b
dmitry <dmitry_romanof@mail.ru>
2025-02-18 18:14:13 +0500

    correction

--- src/widget.py
+++ src/widget.py
@@ -7,12 +7,17 @@ def mask_account_card(input_string: str) -> str:
     """
     parts = input_string.split()  # Разделяем входную строку на слова
     account_type = " ".join(parts[:-1])  # Извлекаем тип аккаунта (все слова, кроме последнего)
-    account_number = int(parts[-1])  # Проверка на число
+    account_number = parts[-1]  # Последний элемент — номер карты или счета
 
-    if "Visa" in account_type or "Maestro" in account_type or "Mastercard" in account_type:
-        return f"{account_type} {get_mask_card_number(account_number)}"
+    # Проверяем, является ли номер числом
+    if not account_number.isdigit():
+        raise ValueError("Номер карты или счета должен состоять из цифр.")
+
+    # Определяем, нужно ли маскировать как карту или как счет
+    if "Visa" in account_type or "Maestro" in account_type or "MasterCard" in account_type:
+        return f"{account_type} {get_mask_card_number(int(account_number))}"
     else:
-        return f"{account_type} {get_mask_account(account_number)}"
+        return f"{account_type} {get_mask_account(int(account_number))}"
 
 
 def get_date(date_string: str) -> str:
